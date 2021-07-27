5a0901d26091cc12ec8e73da09f38963d85bb75a
Jeff Grant <jgrant274@gmail.com>
2021-07-27 01:26:38 -0400

    login & signup page

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Auth from '../../utils/auth';
-import { GiGavel } from 'react-icons/gi'
+import React from "react";
+import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
+import { GiGavel } from "react-icons/gi";
 
 const Header = () => {
-
-  const logout = event => {
+  const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
 
   return (
     <header>
-        <Link to="/">
-          <h1><GiGavel/>myBid</h1>
-        </Link>
+      <Link to="/">
+        <h1>
+          <GiGavel />
+          myBid
+        </h1>
+      </Link>
 
-        <nav>
-          {Auth.loggedIn() ? (
-            <>
-              <Link to="/profile">Profile</Link>
-              <a href="/" onClick={logout}>
-                Logout
-              </a>
-            </>
-          ) : (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-            </>
-          )}
-        </nav>
+      <nav>
+        {Auth.loggedIn() ? (
+          <>
+            <Link to="/profile">Profile</Link>
+            <a href="/" onClick={logout}>
+              Logout
+            </a>
+          </>
+        ) : (
+          <>
+            <ul className="right hide-on-med-and-down">
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+            </ul>
+          </>
+        )}
+      </nav>
     </header>
   );
 };
