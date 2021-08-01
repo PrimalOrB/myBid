fcbb4f122ddcc6752eac3547e6c803e8190cd9ed
Andrew Ogilvie <primalorb@gmail.com>
2021-08-01 19:17:09 -0400

    add paid status to auction model, and button to push for unpaid auctions which we will connect with cart

--- client/src/components/OwnedBidCard/index.js
+++ client/src/components/OwnedBidCard/index.js
@@ -12,6 +12,14 @@ const OwnedBidsCard = ( { bid, auction, user, type }) => {
         return () => clearTimeout(timer);
     });
 
+    function handleAddCart( auction ) {
+      // this is the auction info that will go to the cart. we will not need all of it, probably title, description, _id, and auctionInfo.currentBid (cost))
+      console.log( auction )
+      // after payment, we will need to create a dB route which updates the paid status of the auction
+    }
+
+
+    
   return (
     <>
     { 
@@ -50,7 +58,7 @@ const OwnedBidsCard = ( { bid, auction, user, type }) => {
           :
           <>
           <div className='card'>
-              { user === auction.auctionInfo.currentLeader ? <span className='winning'>Auction Won</span> : <span className='losing'>Auction Lost</span>}
+              { user === auction.auctionInfo.currentLeader ? <span className='winning'>Auction Won { !auction.paid ? <span className='pay-status not-paid' onClick={ () => handleAddCart( auction ) }>Pay Now</span> : <span className='pay-status paid'>Paid</span>}</span> : <span className='losing'>Auction Lost</span>}
               <span className='card-title'>{auction.title} @ <span key={ auction.auctionInfo.currentBid.toFixed(2) } className='bid-value'>${ auction.auctionInfo.currentBid.toFixed(2) }</span></span>
               <div className='card-current'>
               <span>Your Max Bid: <span key={ bid.maxBid.toFixed(2) } className='bid-value'>${ bid.maxBid.toFixed(2) }</span></span>
